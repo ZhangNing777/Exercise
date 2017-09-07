5ca9cb78a71c810eb8bb34e8c2dee82e49c73c9e
ZhangNing777 <30955643+ZhangNing777@users.noreply.github.com>
2017-09-07 22:48:40 +0800

    New_Exercise030
    change

--- Exercise30/src/InsectNumber.java
+++ Exercise30/src/InsectNumber.java
@@ -8,29 +8,35 @@ import java.util.Scanner;
  */
 public class InsectNumber {
     public static void main(String[] args) {
-        int a[] = new int[]{1,2,3,5,7,9,10,12,14,16,19,20};
+        int a[];
+        a = new int[6];
+        a[0] = 1;
+        a[1] = 3;
+        a[2] = 5;
+        a[3] = 7;
+        a[4] = 9;
         Scanner s = new Scanner(System.in);
         System.out.println("Please inout a number:");
         int b = s.nextInt();
-        int c[] = instct(a,b);
-        for(int i=0;i<a.length+1;i++)
+        insect2(a,b);
+        for(int i=0;i<a.length;i++)
         {
-            System.out.print(c[i]+" ");
+            System.out.print(a[i]+" ");
         }
     }
 
-    private static int[] instct(int a[],int b)
+    private static int[] insect1(int a[],int b)
     {
         int m = a.length;
         int c[] = new int[m+1];
-        for(int i=0;i<a.length-1;i++)
+        for(int i=0;i<a.length-2;i++)
         {
             if(a[i]>b) {
                 c[i] = b;
                 for (int j = 0; j < i; j++) {
                     c[j] = a[j];
                 }
-                for (int k = i; k < a.length; k++) {
+                for (int k = i; k < a.length-1; k++) {
                     c[k + 1] = a[k];
                 }
                 break;
@@ -38,4 +44,24 @@ public class InsectNumber {
         }
         return c;
     }
+
+    private static int[] insect2(int a[],int b)
+    {
+        int m = a.length;
+        for(int i=0;i<a.length-1;i++)
+        {
+            if(a[i]>b) {
+                int temp;
+                temp = a[i];
+                a[i] = b;
+                for (int j= a.length-1; j>i+1; j--) {
+                    a[j] =a[j-1];
+                }
+                a[i+1] = temp;
+                break;
+            }
+        }
+        return a;
+    }
+
 }
